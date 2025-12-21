e0cbc9112c47957d980a3cc37c502e1332e1b287
Bao Phan <phangiabao293@gmail.com>
2025-12-21 21:31:57 +0700

    Chats send and history
    send chats. history + timestamp at firebase

--- app/src/main/java/com/example/androidchatapp/ChatActivity.java
+++ app/src/main/java/com/example/androidchatapp/ChatActivity.java
@@ -13,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.androidchatapp.model.ChatMessageModel;
 import com.example.androidchatapp.model.ChatroomModel;
 import com.example.androidchatapp.model.UserModel;
 import com.example.androidchatapp.utils.AndroidUtil;
@@ -20,6 +21,7 @@ import com.example.androidchatapp.utils.FirebaseUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 
 import java.lang.reflect.Array;
@@ -57,9 +59,33 @@ public class ChatActivity extends AppCompatActivity {
         });
         otherUsername.setText(otherUser.getUsername());
 
+        sendMessageBtn.setOnClickListener((v->{
+            String message = messageInput.getText().toString().trim();
+            if (message.isEmpty())
+                return;
+                sendMessageToUser(message);
+        }));
+
         getOrCreateChatroomModel();
     }
 
+    void sendMessageToUser(String message){
+
+        chatroomModel.setLastMessageTimestamp(Timestamp.now());
+        chatroomModel.setLastMessageSenderId(FirebaseUtil.currentUserId());
+        FirebaseUtil.getChatroomReference(chatroomId).set(chatroomModel);
+
+        ChatMessageModel chatMessageModel = new ChatMessageModel(message,FirebaseUtil.currentUserId(),Timestamp.now());
+        FirebaseUtil.getChatroomMessageReference(chatroomId).add(chatMessageModel)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if(task.isSuccessful()){
+                            messageInput.setText("");
+                        }
+                    }
+                });
+    }
     void getOrCreateChatroomModel() {
         FirebaseUtil.getChatroomReference(chatroomId).get().addOnCompleteListener(task ->  {
             if(task.isSuccessful()){
