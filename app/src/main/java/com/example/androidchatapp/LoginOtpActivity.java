c0228d14219fcb26e38ceaf15d536ea2df754477
Ghastov <92448735+Ghastov@users.noreply.github.com>
2025-12-28 03:32:00 +0700

    Update LoginOtpActivity.java

--- app/src/main/java/com/example/androidchatapp/LoginOtpActivity.java
+++ app/src/main/java/com/example/androidchatapp/LoginOtpActivity.java
@@ -6,8 +6,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -16,138 +14,86 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.androidchatapp.utils.AndroidUtil;
+import com.example.androidchatapp.model.UserModel;
+import com.example.androidchatapp.utils.FirebaseUtil;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthOptions;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentSnapshot;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.concurrent.TimeUnit;
+public class LoginUserNameActivity extends AppCompatActivity {
 
-public class LoginOtpActivity extends AppCompatActivity {
-    String phoneNumber;
-    Long timeoutSeconds = 60L;
-    String verificationCode;
-    PhoneAuthProvider.ForceResendingToken resendingToken;
-    EditText otpInput;
-    Button nextBtn;
+    EditText usernameInput;
+    Button letMeInBtn;
     ProgressBar progressBar;
-    TextView resendOtpTextView;
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    String phoneNumber;
+    UserModel userModel;
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_otp);
+        setContentView(R.layout.activity_login_user_name);
 
-        otpInput          = findViewById(R.id.login_otp);
-        nextBtn           = findViewById(R.id.login_next_btn);
-        progressBar       = findViewById(R.id.login_progress_bar);
-        resendOtpTextView = findViewById(R.id.resend_otp_textview);
+        usernameInput = findViewById(R.id.login_username);
+        letMeInBtn = findViewById(R.id.login_let_me_in_btn);
+        progressBar = findViewById(R.id.login_progress_bar);
 
         phoneNumber = getIntent().getExtras().getString("phone");
-
-        sendOtp(phoneNumber,false);
-        nextBtn.setOnClickListener(v->{
-            String enteredOtp = otpInput.getText().toString();
-            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationCode,enteredOtp);
-            signIn(credential);
-            setInProgress(true);
-        });
-
-        resendOtpTextView.setOnClickListener((v)->{
-            sendOtp(phoneNumber,true);
-        });
+        getUsername();
+        letMeInBtn.setOnClickListener((v -> {
+            setUsername();
+        }));
     }
 
-    void sendOtp(String phoneNumber,boolean isResend){
-        startResendTimer();
-        setInProgress(true);
-        PhoneAuthOptions.Builder builder =
-                PhoneAuthOptions.newBuilder(mAuth)
-                        .setPhoneNumber(phoneNumber)
-                        .setTimeout(timeoutSeconds, TimeUnit.SECONDS)
-                        .setActivity(this)
-                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-                            @Override
-                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-                                signIn(phoneAuthCredential);
-                                setInProgress(false);
-                            }
+    void setUsername(){
 
-                            @Override
-                            public void onVerificationFailed(@NonNull FirebaseException e) {
-                                AndroidUtil.showToast(getApplicationContext(),"OTP verification failed");
-                                setInProgress(false);
-                            }
-
-                            @Override
-                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                                super.onCodeSent(s, forceResendingToken);
-                                verificationCode = s;
-                                resendingToken = forceResendingToken;
-                                AndroidUtil.showToast(getApplicationContext(),"OTP sent successfully");
-                                setInProgress(false);
-                            }
-                        });
-        if(isResend){
-            PhoneAuthProvider.verifyPhoneNumber(builder.setForceResendingToken(resendingToken).build());
-        } else {
-            PhoneAuthProvider.verifyPhoneNumber(builder.build());
+        String username = usernameInput.getText().toString();
+        if(username.isEmpty() || username.length()<3){
+             usernameInput.setError("Username length should be at least 3 chars");
+             return;
         }
+        setInProgress(true);
+        if (userModel!=null){
+            userModel.setUsername(username);
+        } else {
+            userModel = new UserModel(phoneNumber,username, Timestamp.now(), FirebaseUtil.currentUserId());
+        }
+
+        FirebaseUtil.currentUserDetails().set(userModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                    intent = new Intent(LoginUserNameActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+    void getUsername(){
+        setInProgress(true);
+        FirebaseUtil.currentUserDetails().get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                   userModel = task.getResult().toObject(UserModel.class);
+                   if (userModel!=null){
+                       usernameInput.setText(userModel.getUsername());
+                   }
+                }
+            }
+        });
     }
     void setInProgress(boolean inProgress){
         if(inProgress){
             progressBar.setVisibility(View.VISIBLE);
-            nextBtn.setVisibility(View.GONE);
+            letMeInBtn.setVisibility(View.GONE);
         } else {
             progressBar.setVisibility(View.GONE);
-            nextBtn.setVisibility(View.VISIBLE);
+            letMeInBtn.setVisibility(View.VISIBLE);
         }
-    }
-
-    void signIn(PhoneAuthCredential phoneAuthCredential){
-        //login and go to next activity
-        setInProgress(true);
-        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Intent intent = new Intent(LoginOtpActivity.this, LoginUserNameActivity.class);
-                    intent.putExtra("phone",phoneNumber);
-                    startActivity(intent);
-                } else {
-                    AndroidUtil.showToast(getApplicationContext(),"OTP verification failed");
-                }
-            }
-        });
-    }
-
-    void startResendTimer(){
-        resendOtpTextView.setEnabled(false);
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                timeoutSeconds--;
-                resendOtpTextView.setText("Resend OTP in "+timeoutSeconds +" second");
-                if(timeoutSeconds <= 0){
-                    timeoutSeconds = 60L;
-                    timer.cancel();
-                    runOnUiThread(() -> {
-                        resendOtpTextView.setEnabled(true);
-                    });
-                }
-            }
-        },0,1000);
     }
 }
