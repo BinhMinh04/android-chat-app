0448bc5611a8e160134bfb395fcc12f55483ea2f
Ghastov <92448735+Ghastov@users.noreply.github.com>
2025-12-28 03:30:19 +0700

    Update ChatFragment.java

--- app/src/main/java/com/example/androidchatapp/ChatFragment.java
+++ app/src/main/java/com/example/androidchatapp/ChatFragment.java
@@ -3,10 +3,20 @@ package com.example.androidchatapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.androidchatapp.adapter.RecentChatRecyclerAdapter;
+import com.example.androidchatapp.adapter.SearchUserRecyclerAdapter;
+import com.example.androidchatapp.model.ChatroomModel;
+import com.example.androidchatapp.model.UserModel;
+import com.example.androidchatapp.utils.FirebaseUtil;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.Query;
 
 public class ChatFragment extends Fragment {
 
@@ -20,8 +30,46 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view =  inflater.inflate(R.layout.fragment_chat, container, false);
-        recyclerView = view.findViewById(R.id.recyler_view);
+        recyclerView = view.findViewById(R.id.recycler_view);
         setupRecyclerView();
 
         return view;
     }
+
+    void setupRecyclerView(){
+
+        Query query = FirebaseUtil.allChatroomCollectionReference()
+                .whereArrayContains("userIds",FirebaseUtil.currentUserId())
+                .orderBy("lastMessageTimestamp",Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<ChatroomModel> options = new FirestoreRecyclerOptions.Builder<ChatroomModel>()
+                .setQuery(query,ChatroomModel.class).build();
+
+        adapter = new RecentChatRecyclerAdapter(options,getContext());
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(adapter!=null)
+            adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(adapter!=null)
+            adapter.stopListening();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(adapter!=null)
+            adapter.notifyDataSetChanged();
+    }
+}
