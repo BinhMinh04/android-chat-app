c9094441f46131abab22179b115a528a2ecffff7
Ghastov <92448735+Ghastov@users.noreply.github.com>
2025-12-28 08:39:55 +0700

    Update SplashActivity.java

--- app/src/main/java/com/example/androidchatapp/SplashActivity.java
+++ app/src/main/java/com/example/androidchatapp/SplashActivity.java
@@ -1,5 +1,8 @@
 package com.example.androidchatapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -11,7 +14,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.androidchatapp.model.UserModel;
+import com.example.androidchatapp.utils.AndroidUtil;
 import com.example.androidchatapp.utils.FirebaseUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -20,17 +30,39 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if(FirebaseUtil.isLoggedIn()){
-                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                } else {
-                    startActivity(new Intent(SplashActivity.this,LoginPhoneNumberActivity.class));
-                }
-                finish();
-            }
-        }, 1000);
+        if(getIntent().getExtras()!=null){
+            //from notification
+            String userId = getIntent().getExtras().getString("userId");
+            FirebaseUtil.allUserCollectionReference().document(userId).get()
+                    .addOnCompleteListener(task -> {
+                        if(task.isSuccessful()){
+                            UserModel model = task.getResult().toObject(UserModel.class);
 
+                            Intent mainIntent = new Intent(this,MainActivity.class);
+                            mainIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                            startActivity(mainIntent);
+
+                            Intent intent = new Intent(this, ChatActivity.class);
+                            AndroidUtil.passUserModelAsIntent(intent,model);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+
+
+        }else {
+            new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    if (FirebaseUtil.isLoggedIn()) {
+                        startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    } else {
+                        startActivity(new Intent(SplashActivity.this, LoginPhoneNumberActivity.class));
+                    }
+                    finish();
+                }
+            }, 1000);
+        }
     }
 }
